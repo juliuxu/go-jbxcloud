2b314178a537153188e9218a7f89573e1454edb5
Julian Jark <juliuxu@gmail.com>
2014-07-07 21:13:34 +0200

    added tests

--- go-jbxcloud_test.go
+++ go-jbxcloud_test.go
@@ -4,28 +4,74 @@ import "testing"
 import "fmt"
 
 // Private Username and Password
+// IMPORTTANT: DO NOT GIT COMMIT AND/OR PUSH UP WHILE THESE LINES ARE NOT EMPTY!!!
 var username string = ""
 var password string = ""
 
 func TestAvaiable(t *testing.T) {
 
-    joebox := Client{Username: username, Password: password}
-    joebox.UseDefaultUrl()
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
 
-    if a, err := joebox.IsAvaliable(); err != nil {
-        t.Errorf("Got err: ", err)
-    } else {
-        fmt.Printf("Joebox is available?: %v\n", a)
-    }
+	if _, err := joebox.IsAvaliable(); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("Joebox is available?: %v\n", a)
+	}
 
 }
 
 func TestSystems(t *testing.T) {
-    joebox := Client{Username: username, Password: password}
-    joebox.UseDefaultUrl()
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
 
-    if _, err := joebox.GetSystems(); err != nil {
-        t.Errorf("Got err: ", err)
-    } else {
-    }
+	if _, err := joebox.GetSystems(); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("response: %v\n", v)
+	}
+}
+
+func TestSize(t *testing.T) {
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
+
+	if _, err := joebox.GetSize(); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("response: %v\n", v)
+	}
+}
+
+func TestListAnalysis(t *testing.T) {
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
+
+	if _, err := joebox.ListAnalyses(); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("response: %v\n", v)
+	}
+}
+
+func TestCheckAnalysis(t *testing.T) {
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
+
+	if _, err := joebox.CheckAnalysis("48284"); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("response: %v\n", v)
+	}
+}
+
+func TestGetAnalysisResults(t *testing.T) {
+	joebox := Client{Username: username, Password: password}
+	joebox.UseDefaultUrl()
+
+	if _, err := joebox.GetAnalysisResults("47865"); err != nil {
+		t.Errorf("Got err: %v", err)
+	} else {
+		// fmt.Printf("response: %v\n", v)
+	}
 }
